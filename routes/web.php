2b78f57355819b251bdf6c2b8d4ef20fe0bbc7c3
hendriktarigann01 <hendriktarigan25@gmail.com>
2024-12-05 23:29:36 +0700

    update routing

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,19 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
+Route::view('home', 'home')
+    ->name('home');
+
+Route::view('blog', 'blog')
+    ->name('blog');
+
+Route::view('category', 'category')
+    ->name('category');
+
+Route::view('contact', 'contact')
+    ->name('contact');
+
+Route::view('faq', 'faq')
+    ->name('faq');
+
 require __DIR__.'/auth.php';
