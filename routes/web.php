a03a2e96902892599b44638b1da411b4893f8c64
hendriktarigann01 <hendriktarigan25@gmail.com>
2024-12-08 22:35:17 +0700

    new route post

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ViewPostController;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
+Route::get('dashboard', [ViewPostController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
@@ -13,11 +14,16 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
+// navbar
 Route::view('home', 'home')->name('home');
 Route::view('blog', 'blog')->name('blog');
 Route::view('category', 'category')->name('category');
 Route::view('contact', 'contact')->name('contact');
 Route::view('faq', 'faq')->name('faq');
+
+Route::get('/create', function () {
+    return view('livewire.posts.create-post');
+})->middleware(['auth'])->name('posts.create');
 
 Route::post('/posts', [PostController::class, 'store'])->middleware('auth')->name('posts.dashboard');
 
