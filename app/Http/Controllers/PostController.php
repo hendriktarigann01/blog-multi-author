e629f7d308e7d13e3890a698623b37436a10d03b
hendriktarigann01 <hendriktarigan25@gmail.com>
2024-12-08 22:27:46 +0700

    new controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class PostController extends Controller
 {
@@ -15,7 +16,16 @@ class PostController extends Controller
             'post_type' => 'required|string|max:255',
             'post_content' => 'required|string',
             'post_description' => 'required|string',
+            'post_image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
+
+        if ($request->hasFile('post_image')) {
+            $imageName = time() . '.' . $request->post_image->extension();
+            $request->post_image->move(public_path('images/posts'), $imageName);
+            $validated['post_image'] = $imageName;
+        } else {
+            $validated['post_image'] = null; // Jika tidak ada gambar, biarkan null
+        }
 
         $validated['post_users_id'] = Auth::id();
 
---
+++ app/Http/Controllers/ViewPostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Post;
+
+class ViewPostController extends Controller
+{
+    public function index()
+    {
+        // Ambil data post berdasarkan pengguna yang login
+        $posts = Post::where('post_users_id', Auth::id())->get();
+
+        return view('dashboard', compact('posts'));
+    }
+}
