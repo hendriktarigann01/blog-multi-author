b5ee91b12ca1ce30a740be436f8c29ce748c9d5f
hendriktarigann01 <hendriktarigan25@gmail.com>
2024-12-17 00:36:28 +0700

    pagination ui

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -15,7 +15,7 @@
             </div>
         </div>
         <div class="blog-header-container">
-            @foreach ($posts as $post)
+            @foreach ($posts as $index => $post)
             <div class="blog-header">
                 <div class="blog-article header-article">
                     <div class="title-mobile">Article</div>
@@ -34,12 +34,68 @@
                         </svg>
                         See More
                     </a>
+                    @if ($loop->last)
+                    <div class="small-title"></div>
+                    <nav class="flex items-center gap-x-1 justify-center"
+                        aria-label="Pagination">
+                        <button type="button"
+                            class="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg border border-transparent hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-transparent text-cyan-800 dark:hover:bg-white/10 dark:focus:bg-white/10"
+                            aria-label="First">
+                            <span class="sr-only">First</span>
+                            <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M17 18l-6-6 6-6"></path>
+                                <path d="M7 18l-6-6 6-6"></path>
+                            </svg>
+                        </button>
+                        <button type="button"
+                            class="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg border border-transparent hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-transparent text-cyan-800 dark:hover:bg-white/10 dark:focus:bg-white/10"
+                            aria-label="Previous">
+                            <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path d="m15 18-6-6 6-6"></path>
+                            </svg>
+                            <span class="sr-only">Previous</span>
+                        </button>
+                        <div class="flex items-center gap-x-1">
+                            <button type="button"
+                                class="min-h-[38px] min-w-[38px] flex justify-center items-center border border-gray-200 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 text-cyan-800 dark:focus:bg-white/10"
+                                aria-current="page">1</button>
+                            <button type="button"
+                                class="min-h-[38px] min-w-[38px] flex justify-center items-center border border-transparent hover:bg-gray-100 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-transparent text-cyan-800 dark:hover:bg-white/10 dark:focus:bg-white/10">2</button>
+                            <button type="button"
+                                class="min-h-[38px] min-w-[38px] flex justify-center items-center border border-transparent hover:bg-gray-100 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-transparent text-cyan-800 dark:hover:bg-white/10 dark:focus:bg-white/10">3</button>
+                        </div>
+                        <button type="button"
+                            class="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg border border-transparent hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-transparent text-cyan-800 dark:hover:bg-white/10 dark:focus:bg-white/10"
+                            aria-label="Next">
+                            <span class="sr-only">Next</span>
+                            <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path d="m9 18 6-6-6-6"></path>
+                            </svg>
+                        </button>
+                        <button type="button"
+                            class="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg border border-transparent hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-transparent text-cyan-800 dark:hover:bg-white/10 dark:focus:bg-white/10"
+                            aria-label="Last">
+                            <span class="sr-only">Last</span>
+                            <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M7 6l6 6-6 6"></path>
+                                <path d="M17 6l6 6-6 6"></path>
+                            </svg>
+                        </button>
+                    </nav>
+                    <p class="flex justify-center">Showing 1 to 3 of 3 entries</p>
+                    @endif
                 </div>
             </div>
             @endforeach
         </div>
 
-       <div class="blog-part right-blog">
+        <div class="blog-part right-blog">
             <marquee width="100%" direction="left">
                 <span>Now And Then You Miss It Sounds Make You Cry</span>
                 <span>Now In - MoMa Sharing Exhibition NOW</span>
