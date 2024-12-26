062d6af6f7282c2b851657b17f4cc0f63d8be65a
hendriktarigann01 <hendriktarigan25@gmail.com>
2024-12-27 01:55:32 +0700

    new column post_views

---
+++ database/migrations/2024-12-05-124551_create_posts_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /** * Run the migrations. */ public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_users_id');
+            $table->string('post_title');
+            $table->string('post_category_id');
+            $table->text('post_description');
+            $table->string('post_image')->nullable();
+            $table->integer('post_views')->default(0);
+            $table->timestamps();
+        });
+    }
+    /** * Reverse the migrations. */ public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
