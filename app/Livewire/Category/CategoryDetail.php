a4dd9406d98c3391631251f83563fa533c102065
hendriktarigann01 <hendriktarigan25@gmail.com>
2025-01-05 00:08:14 +0700

    top view post in category detail

--- app/Livewire/Category/CategoryDetail.php
+++ app/Livewire/Category/CategoryDetail.php
@@ -9,16 +9,19 @@ use App\Models\Post;
 class CategoryDetail extends Component
 {
     public $category;
+    public $topViewedPosts;
 
     public function mount($id)
     {
         $this->category = Category::findOrFail($id);
+
+        $this->topViewedPosts = Post::orderBy('post_views', 'desc')->limit(3)->get();
     }
-
-
 
     public function render()
     {
-        return view('livewire.category.category-detail')->layout('layouts.app');
+        return view('livewire.category.category-detail', [
+            'topViewedPosts' => $this->topViewedPosts
+        ])->layout('layouts.app');
     }
 }
