9cb2360e6c2da43adbbbd6d5e3ccf535079dd1da
hendriktarigann01 <hendriktarigan25@gmail.com>
2025-01-08 23:53:10 +0700

    navigate mobile ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,24 @@ import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 
 
+// navigation mobile
+document.addEventListener("DOMContentLoaded", () => {
+    const menuIcon = document.getElementById("menu-icon");
+    const closeIcon = document.getElementById("close-icon");
+    const navigation = document.getElementById("navigation");
+
+    menuIcon.addEventListener("click", () => {
+        menuIcon.classList.add("hidden");
+        closeIcon.classList.remove("hidden");
+        navigation.classList.add("visible");
+        navigation.classList.remove("hidden");
+    });
+
+    closeIcon.addEventListener("click", () => {
+        closeIcon.classList.add("hidden");
+        menuIcon.classList.remove("hidden");
+        navigation.classList.remove("visible");
+        navigation.classList.add("hidden");
+    });
+});
+
--- resources/views/livewire/navbar/navigation.blade.php
+++ resources/views/livewire/navbar/navigation.blade.php
@@ -1,29 +1,22 @@
 <nav>
     @auth
-    <a href="{{ url('/dashboard') }}" class="blog-menu hover:text-cyan-600 inline-block">
+    <a href="{{ url('/dashboard') }}" class="blog-menu text-center hover:text-cyan-600 my-2">
         Dashboard
-
     </a>
     @else
-    <a href="{{ route('home') }}" class="blog-menu hover:text-cyan-600 inline-block">
+    <a href="{{ route('home') }}" class="blog-menu text-center hover:text-cyan-600 my-2">
         Home
-
     </a>
-    <a href="{{ route('article') }}" class="blog-menu hover:text-cyan-600 inline-block">
+    <a href="{{ route('article') }}" class="blog-menu text-center hover:text-cyan-600 my-2">
         Article
-
     </a>
-    <a href="{{ route('login') }}" class="blog-menu hover:text-cyan-600 inline-block">
+    <a href="{{ route('login') }}" class="blog-menu text-center hover:text-cyan-600 my-2">
         Log in
-
     </a>
-
     @if (Route::has('register'))
-    <a href="{{ route('register') }}" class="blog-menu hover:text-cyan-600 inline-block">
+    <a href="{{ route('register') }}" class="blog-menu text-center hover:text-cyan-600 my-2">
         Register
-
     </a>
     @endif
     @endauth
 </nav>
-
