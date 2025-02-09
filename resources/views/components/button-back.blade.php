397b50a004e1eade97dc6f19a5a43311dc0f457a
hendriktarigann01 <hendriktarigan25@gmail.com>
2025-02-10 01:24:46 +0700

    new button back component

---
+++ resources/views/components/button-back.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <button
+        class="flex border border-[#94918f] text-[#94918f] justify-center items-center w-10 h-10 rounded hover:border-cyan-800 hover:text-cyan-800 transition duration-200 relative">
+        <i class="fa-solid fa-angles-left"></i>
+    </button>
+
+    <span
+        class="absolute -top-8 left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 bg-gray-800 text-white text-xs rounded py-1 px-2 transition duration-200">
+        Back
+    </span>
+</div>
